0f30b5324a22706c2503354044cda02ea41a2e5e
simon@szdanqing.com <simon@szdanqing.com>
2019-02-21 14:42:15 +0800

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HelloWorld/>
   </div>
 </template>
 
@@ -17,12 +16,8 @@ export default {
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+*{
+  margin: 0;
+  padding: 0;
 }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,132 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <div class="uploadIptBox" v-show="isHide == true">
+      <input class="uploadIpt" type="file" @change="select($event)">
+      <div class="uploadDom">选择图片</div>
+    </div>
+    <canvas id="imgCanvas"></canvas>
+    <canvas id="retCanvas"></canvas>
+    <p>{{resopnseData}}</p>
   </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
+  data(){
+    return {
+      isHide:true,
+      imgData:"",
+      resopnseData:""
+    }
+  },
+  methods:{
+    select:function (e){
+      this.isHide = false;
+      this.imgData = e.target.files[0];
+      var imgCanvas = document.getElementById("imgCanvas"),
+          canvasImg = imgCanvas.getContext("2d");
+
+      var img = new Image();
+      var reader = new FileReader();
+      reader.readAsDataURL(this.imgData);
+      reader.onload = function (e) {
+        img.crossOrigin = "anonymous";
+        img.src = e.target.result;
+      }
+
+      img.onload = function(){
+        var imgWidth  = img.width || img.naturalWidth,
+            imgHeight = img.height || img.naturalHeight;
+        imgCanvas.width = 500;
+        imgCanvas.height = imgHeight * (500/imgWidth);
+        canvasImg.drawImage(img,0,0,imgCanvas.width,imgCanvas.height);
+
+        var retCanvas = document.getElementById("retCanvas"),
+            canvasRet = retCanvas.getContext("2d"),
+            cutData = canvasImg.getImageData(0,0,imgCanvas.width,imgCanvas.height),
+            flag = false,
+            startX = 0,
+            startY = 0;
+        imgCanvas.addEventListener("mousedown",function(e){
+          flag = true;
+          startX = e.clientX;
+          startY = e.clientY;
+        });
+        imgCanvas.addEventListener("mousemove",function(e){
+          if(flag){
+            canvasImg.clearRect(0,0,imgCanvas.width,imgCanvas.height);
+            canvasRet.clearRect(0,0,cutData.width,cutData.height);
+
+            canvasImg.drawImage(img,0,0,imgCanvas.width,imgCanvas.height);
+            canvasImg.fillStyle = 'rgba(0,0,0,.5)';
+            canvasImg.fillRect(0,0,e.clientX,startY);
+            canvasImg.fillRect(e.clientX,0,imgCanvas.width,e.clientY);
+            canvasImg.fillRect(startX,e.clientY,imgCanvas.width-startX,imgCanvas.height-e.clientY);
+            canvasImg.fillRect(0,startY,startX,imgCanvas.height-startY);
+
+            var canvasRetW = null,canvasRetH = null;
+            if(e.clientX - startX < 0 || e.clientX - startX == 0){
+              canvasRetW = 1;
+            }else{
+              canvasRetW = e.clientX - startX;
+            }
+            if(e.clientY - startY < 0 || e.clientY - startY == 0){
+              canvasRetH = 1;
+            }else{
+              canvasRetH = e.clientY - startY;
+            }
+            retCanvas.width = canvasRetW;
+            retCanvas.height = canvasRetH;
+            var RetCutData = canvasImg.getImageData(startX,startY,canvasRetW,canvasRetH);
+            canvasRet.putImageData(RetCutData,0,0);
+          }
+        })
+        imgCanvas.addEventListener("mouseup",function(){
+          flag = false;
+          retCanvas.toBlob(function(blob){
+            var formData = new FormData();
+            formData.append("imgnum",blob);
+            var httpDemo =  new XMLHttpRequest();
+            httpDemo.open("post","https://user.intechtrading.com/api/Overseas/setOpenAcountInfo.ashx?action=addressphoto&proxyid=160&usertoken=DiB4pC5S2kL2jzGK4dkGB8rej9PVlVICxhnLihj5HUC6u5dGfzEaOSDJbJUOk8PjTOxxfMQxqAOjYkn6R6GMfQgPAEHArsKewycYkCNff2Wz7MqCuTaKJw**&lang=en",true);
+            httpDemo.onreadystatechange = function(){
+              if(httpDemo.readyState == 4 && httpDemo.status == 200){
+                var data = JSON.parse(httpDemo.responseText);
+                this.resopnseData = data;
+              }
+            };
+            httpDemo.send(formData);
+          })
+        })
+      }
+    }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.uploadIptBox{
+  position: relative;
+  width: 200px;
+  height: 40px;
+  cursor: pointer;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.uploadIpt{
+  display: block;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  font-size: 30px;
+  opacity: 0;
+  cursor: pointer;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+.uploadDom{
+  width: 100%;
+  height: 100%;
+  background-color: #2f7ce8;
+  color: #fff;
+  text-align: center;
+  line-height: 40px;
+  cursor: pointer;
 }
 </style>
